e8ff494edb7ce5e8a81e7edc506dbf1016e56c5c
Arsh <arshgoyal2005@gmail.com>
2022-02-06 13:09:43 +0530

    added NavBar and functionality to toggle theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './components/styles/App.scss';
+import React, { useState, useEffect } from "react";
+import "./components/styles/App.scss";
 
-function App() {
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? true : false
+  );
+
+  useEffect(() => {
+    darkMode
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  }, [darkMode]);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import "./styles/NavBar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+
+const NavBar = ({ darkMode, setDarkMode }) => {
+  const clickHandler = () => {
+    darkMode ? setDarkMode(false) : setDarkMode(true);
+  };
+
+  return (
+    <nav>
+      <h1>Where in the world?</h1>
+      <button className="theme-toggle-btn" onClick={clickHandler}>
+        {!darkMode ? (
+          <>
+            <FontAwesomeIcon icon={faMoon} /> <span>Dark Mode</span>
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faSun} /> <span>Light Mode</span>
+          </>
+        )}
+      </button>
+    </nav>
+  );
+};
+
+export default NavBar;
