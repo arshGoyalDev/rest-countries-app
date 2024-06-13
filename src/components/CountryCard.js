36859b5dc762423d700263f17ca0393df340f122
Arsh Goyal <arshgoyalDev@gmail.com>
2024-06-13 16:30:53 +0530

    improved box-shadow styles

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -3,30 +3,32 @@ import { Link } from "react-router-dom";
 
 const Country = ({ data }) => {
   return (
-    <div className="country-card">
-      <div className="country-card--flag">
-        <img src={data.flags.png} alt={data.name} />
-      </div>
-      <div className="country-card--details">
-        <Link to={`/${data.alpha3Code}`}>
+    <Link to={`/${data.alpha3Code}`}>
+      <div className="country-card">
+        <div className="country-card--flag">
+          <img src={data.flags.png} alt={data.name} />
+        </div>
+        <div className="country-card--details">
           <h3>{data.name}</h3>
-        </Link>
-        <div className="country-card--details--other">
-          <p>
-            Population :{" "}
-            <span>
-              {data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            </span>
-          </p>
-          <p>
-            Region : <span>{data.region}</span>
-          </p>
-          <p>
-            Capital : <span>{data.capital}</span>
-          </p>
+          <div className="country-card--details--other">
+            <p>
+              Population :{" "}
+              <span>
+                {data.population
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </span>
+            </p>
+            <p>
+              Region : <span>{data.region}</span>
+            </p>
+            <p>
+              Capital : <span>{data.capital}</span>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
